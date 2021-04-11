fca3d9212812a4b24937809e332aa83340614126
YunPC <apsn2000@nate.com>
2021-04-11 14:57:26 +0900

    feat: Add searching using in
    Optimize solution with in

--- List/two-sum.py
+++ List/two-sum.py
@@ -1,4 +1,7 @@
-def solution(nums, target):
+
+# 부르트 포스로 계산
+
+def solution1(nums, target):
     answer = []
     length = len(nums)
     for i in range(length):
@@ -8,4 +11,18 @@ def solution(nums, target):
 
     return answer
 
-print(solution([2, 7, 11, 15], 9))
+print(solution1([2, 7, 11, 15], 9))
+
+# in을 이용한 탐색
+
+def solution2(nums, target):
+    answer = []
+    length = len(nums)
+    for i, n in enumerate(nums):
+        complement = target - n
+        
+        if complement in nums[i+1:]:
+            return nums.index(n), nums[i+1:].index(complement) + (i+1)
+
+    return answer
+
